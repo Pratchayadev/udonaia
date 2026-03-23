a6301e48643a236e11376d7e2aa8f9b55d1caeca
pratchaya3party <pratchaya3party@gmail.com>
2026-03-23 12:44:21 +0700

    update

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="min-w-0">
+    <SpeedInsights />
     <PageLoadingScreen />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/vue'
+</script>
